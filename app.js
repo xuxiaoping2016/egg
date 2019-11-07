9eaa42d1186e672f0788bc4214154bd4216b8e3c
xiaoping.xu <xiaoping.xu@weimob.com>
2019-11-07 19:05:46 +0800

    koa-view

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-const Koa = require('koa');
+const nunjucks = require('nunjucks');
+let staticFiles = require('./static-files');
+app.use(staticFiles('/static/', __dirname + '/static'))
 
-const bodyParser = require('koa-bodyparser');
+const isProduction = process.env.NODE_ENV === 'production';
 
-const controller = require("./controller")
-// 创建一个Koa对象表示web app本身:
-const app = new Koa();
-
-
-app.use(bodyParser());
-// add router middleware:
-app.use(controller());
-
-// 在端口3000监听:
-app.listen(3001);
-console.log('app started at port 3001...');
+app.use(templating('views', {
+    noCache: !isProduction,
+    watch: !isProduction
+}));
